cb8d07e18207403911bbc363254a0d01e02d1159
Anuj Mahajan <anujmaha@adobe.com>
2025-02-10 21:49:01 +0530

    fix: update styling for blocks in article page

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -6,16 +6,32 @@ export default function decorate(block) {
     template, readTime, author, tag, date,
   } = getArticleRelatedMetadata();
   if (isLabelsVariant && template === 'article') {
+    const h1 = block.querySelector('h1');
     const labelsWrapper = document.createElement('div');
     labelsWrapper.classList.add('labels-wrapper');
+    /**
+     * Create a wrapper for the labels and the title
+     * This is to ensure that the labels and the title are aligned properly
+     * and the title is centered
+     */
     labelsWrapper.innerHTML = `
       <div class="row">
-        <span class="article-time"><span class="icon icon-list"></span>${readTime} READ</span>
+        <span class="article-time">
+          <span class="icon icon-list">
+            <img data-icon-name="list" src="/icons/list.svg" alt="" loading="lazy">
+          </span>
+          ${readTime} READ
+        </span>
         <span class="article-featured-tag">${tag}</span>
         <a class="bookmark" href="#">
-          <span id="tooltip-saved" class="icon icon-bookmark-outlined"></span>
+          <span id="tooltip-saved" class="icon icon-bookmark-outlined">
+            <img data-icon-name="bookmark" src="/icons/bookmark-outlined.svg" alt="" loading="lazy">
+          </span>
           <span class="save-text">Save</span>
         </a>
+      </div>
+      <div class="row article-title">
+        ${h1.outerHTML}
       </div>
        <div class="row">
         <span class="authors">By ${author}</span>
@@ -24,6 +40,7 @@ export default function decorate(block) {
       </div>
     `;
     block.prepend(labelsWrapper);
+    h1.remove();
 
     const bookmark = document.querySelector('.hero.labels .row .bookmark');
     const tooltip = document.querySelector('#tooltip-saved');
