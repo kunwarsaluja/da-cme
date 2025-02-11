0ad30a7379fab1cdfa76f91756df99925d2a0976
Anuj Mahajan <anujmaha@adobe.com>
2025-02-11 11:19:39 +0530

    fix: refactoring hero code

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,60 +1,59 @@
-import { getArticleRelatedMetadata } from '../../scripts/utils.js';
+import { createElement, getArticleRelatedMetadata } from '../../scripts/utils.js';
 
-export default function decorate(block) {
-  const isArticleVariant = block.classList.contains('article');
+function decorateArticlePageHero(block) {
   const {
     readTime, author, tag, date,
   } = getArticleRelatedMetadata();
-  if (isArticleVariant) {
-    const h1 = block.querySelector('h1');
-    const labelsWrapper = document.createElement('div');
-    labelsWrapper.classList.add('labels-wrapper');
-    /**
-     * Create a wrapper for the labels and the title
-     * This is to ensure that the labels and the title are aligned properly
-     * and the title is centered
-     */
-    labelsWrapper.innerHTML = `
-      <div class="row">
-        <span class="article-time">
-          <span class="icon icon-list">
-            <img data-icon-name="list" src="/icons/list.svg" alt="" loading="lazy">
-          </span>
-          ${readTime} READ
-        </span>
-        <span class="article-featured-tag">${tag}</span>
-        <a class="bookmark" href="#">
-          <span class="tooltip-saved show icon icon-bookmark-outlined">
-            <img data-icon-name="bookmark" src="/icons/bookmark-outlined.svg" alt="" loading="lazy">
-          </span>
-          <span class="tooltip-saved icon icon-bookmark-filled">
-            <img data-icon-name="bookmark" src="/icons/bookmark-filled.svg" alt="" loading="lazy">
-          </span>
-          <span class="save-text">Save</span>
-        </a>
-      </div>
-      <div class="row article-title">
-        ${h1.outerHTML}
-      </div>
-       <div class="row">
-        <span class="authors">By ${author}</span>
-        <span></span>
-        <span class="article-date">${date}</span>
-      </div>
-    `;
-    block.prepend(labelsWrapper);
-    h1.remove();
+  const h1 = block.querySelector('h1');
+  const readIcon = createElement('img', {
+    src: '/icons/list.svg',
+    alt: 'Read Time',
+    loading: 'lazy',
+  });
+  const readIconSpan = createElement('span', { class: 'icon icon-list' }, readIcon);
+  const readTimeText = createElement('span', null, `${readTime} READ`);
+  const articleTime = createElement('span', { class: 'article-time' }, readIconSpan, readTimeText);
+  const featuredTag = createElement('span', { class: 'article-featured-tag' }, tag);
+  const saveIconOutlined = createElement('img', {
+    src: '/icons/bookmark-outlined.svg',
+    alt: 'Bookmark Icon',
+    loading: 'eager',
+  });
+  const saveIconOutlinedSpan = createElement('span', { class: 'show icon icon-bookmark-outlined' }, saveIconOutlined);
+  const saveIconFilled = createElement('img', {
+    src: '/icons/bookmark-filled.svg',
+    alt: 'Bookmark Icon',
+    loading: 'lazy',
+  });
+  const saveIconFilledSpan = createElement('span', { class: 'icon icon-bookmark-filled' }, saveIconFilled);
+  const saveText = createElement('span', { class: 'save-text' }, 'Save');
+  const bookmarkButton = createElement('a', { class: 'bookmark' }, saveIconOutlinedSpan, saveIconFilledSpan, saveText);
+  const row1 = createElement('div', { class: 'row' }, articleTime, featuredTag, bookmarkButton);
+  const row2 = createElement('div', { class: 'row article-title' }, h1);
+  const authorText = `By ${author}`;
+  const authors = createElement('span', { class: 'authors' }, authorText);
+  const articleDate = createElement('span', { class: 'article-date' }, date);
+  const row3 = createElement('div', { class: 'row' }, authors, articleDate);
 
-    const bookmark = document.querySelector('.hero.article .row .bookmark');
-    const tooltips = document.querySelectorAll('.tooltip-saved');
-    bookmark.addEventListener('mouseenter', () => {
-      tooltips.forEach((tooltip) => { tooltip.classList.toggle('show'); });
-    });
-    bookmark.addEventListener('mouseleave', () => {
-      tooltips.forEach((tooltip) => { tooltip.classList.toggle('show'); });
-    });
-    bookmark.addEventListener('click', () => {
-      // TODO: Add bookmark functionality
-    });
+  const labelsWrapper = createElement('div', { class: 'labels-wrapper' }, row1, row2, row3);
+  block.append(labelsWrapper);
+
+  const bookmark = block.querySelector('.bookmark');
+  const saveIcons = bookmark.querySelectorAll('.icon');
+  bookmark.addEventListener('mouseenter', () => {
+    saveIcons.forEach((saveIcon) => { saveIcon.classList.toggle('show'); });
+  });
+  bookmark.addEventListener('mouseleave', () => {
+    saveIcons.forEach((saveIcon) => { saveIcon.classList.toggle('show'); });
+  });
+  bookmark.addEventListener('click', () => {
+    // TODO: Add bookmark functionality
+  });
+}
+
+export default function decorate(block) {
+  const isArticleVariant = block.classList.contains('article');
+  if (isArticleVariant) {
+    decorateArticlePageHero(block);
   }
 }
